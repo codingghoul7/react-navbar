02e740d9d351b411e92d7a8899c09c1f89f9dbc0
CodingGhoul <108980953+codingghoul7@users.noreply.github.com>
2024-03-28 10:07:42 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,24 @@
+
+import "./Navbar.css"
+
+import React, { Component } from 'react'
+
+
+
+export default function Navbar() {
+  return (
+    <>
+   <nav className="nav">
+   <a href="" className="site-title">News Monkey</a>
+   <ul>
+   <li><a href="" className="active">Home</a></li>
+   <li><a href="">About</a></li>
+   <li><a href="">Shop</a></li>
+   <li><a href="">Main News</a></li>
+   </ul>
+   </nav>
+    
+    </>
+    
+  )
+}
